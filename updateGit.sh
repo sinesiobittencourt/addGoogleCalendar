6631d4e0b0e5c3dd3c69291d9dd103cdce901774
sinesiobittencourt <contato@sinesio.com.br>
2019-09-03 16:50:47 -0300

    Update

--- updateGit.sh
+++ updateGit.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 
-#https://github.com/sinesiobittencourt/roleta
 git pull origin master
 echo "*** Update git done. ***"
 git add *
 git commit -m 'Update'
 echo "*** Update git done. ***"
 git push origin master
+
+git config --global credential.helper "cache --timeout 7200"
